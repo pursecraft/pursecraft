fe8c55efa8e96fcfe5a8a5076d4ea05ffc5ac5df
Terence Ponce <terence@pursecraft.com>
2024-12-21 22:58:39 +0800

    Fix version of dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PurseCraft.MixProject do
     [
       app: :purse_craft,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "1.18.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
@@ -32,17 +32,13 @@ defmodule PurseCraft.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.18"},
-      {:phoenix_ecto, "~> 4.5"},
-      {:ecto_sql, "~> 3.10"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.0.0"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
+      {:bandit, "1.6.1"},
+      {:dns_cluster, "0.1.3"},
+      {:ecto_sql, "3.12.1"},
+      {:esbuild, "0.8.2", runtime: Mix.env() == :dev},
+      {:finch, "0.19.0"},
+      {:floki, "0.37.0", only: :test},
+      {:gettext, "0.26.2"},
       {:heroicons,
        github: "tailwindlabs/heroicons",
        tag: "v2.1.1",
@@ -50,14 +46,18 @@ defmodule PurseCraft.MixProject do
        app: false,
        compile: false,
        depth: 1},
-      {:swoosh, "~> 1.5"},
-      {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 1.0"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.26"},
-      {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:jason, "1.4.4"},
+      {:phoenix, "1.7.18"},
+      {:phoenix_ecto, "4.6.3"},
+      {:phoenix_html, "4.1.1"},
+      {:phoenix_live_reload, "1.5.3", only: :dev},
+      {:phoenix_live_view, "1.0.1"},
+      {:postgrex, "0.19.3"},
+      {:phoenix_live_dashboard, "0.8.5"},
+      {:swoosh, "1.17.5"},
+      {:tailwind, "0.2.4", runtime: Mix.env() == :dev},
+      {:telemetry_metrics, "1.0.0"},
+      {:telemetry_poller, "1.1.0"}
     ]
   end
 
