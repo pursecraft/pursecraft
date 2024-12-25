b83e5dd8d89d980e7d3666e08aeb2006b97f31b8
Terence Ponce <terence@pursecraft.com>
2024-12-25 12:28:53 +0800

    Integrate Dialyzer (#8)

--- mix.exs
+++ mix.exs
@@ -17,6 +17,13 @@ defmodule PurseCraft.MixProject do
         "coveralls.post": :test,
         "coveralls.html": :test,
         "coveralls.cobertura": :test
+      ],
+      dialyzer: [
+        plt_add_apps: [
+          :ex_unit
+        ],
+        plt_file: {:no_warn, "priv/plts/project.plt"},
+        list_unused_filter: true
       ]
     ]
   end
@@ -42,6 +49,7 @@ defmodule PurseCraft.MixProject do
     [
       {:bandit, "1.6.1"},
       {:credo, "1.7.10", only: [:dev, :test], runtime: false},
+      {:dialyxir, "1.4.5", only: [:dev, :test], runtime: false},
       {:dns_cluster, "0.1.3"},
       {:ecto_sql, "3.12.1"},
       {:esbuild, "0.8.2", runtime: Mix.env() == :dev},
