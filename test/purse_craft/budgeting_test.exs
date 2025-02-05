bd47764845fa05e4505ecdd81cd7f937e903fc44
Terence Ponce <terence@pursecraft.com>
2025-02-05 09:38:21 +0800

    Rearrange tests

--- test/purse_craft/budgeting_test.exs
+++ test/purse_craft/budgeting_test.exs
@@ -7,51 +7,88 @@ defmodule PurseCraft.BudgetingTest do
   alias PurseCraft.Budgeting.Schemas.Book
   alias PurseCraft.Repo
 
-  describe "books" do
-    @invalid_attrs %{name: nil}
-
-    test "list_books/0 returns all books" do
+  describe "list_books/0" do
+    test "with existing Book record return a list of Book" do
       book = insert(:book)
       assert Budgeting.list_books() == [book]
     end
 
-    test "get_book/1 returns the book with given id" do
+    test "with no existing Book record return empty list" do
+      assert Budgeting.list_books() == []
+    end
+  end
+
+  describe "get_book/1" do
+    test "with existing book returns the book with the given id" do
       book = insert(:book)
       assert Budgeting.get_book(book.id) == book
     end
 
-    test "create_book/1 with valid data creates a book" do
-      valid_attrs = %{name: "some name"}
+    test "with no existing book returns nil" do
+      refute Budgeting.get_book(123)
+    end
+  end
 
-      assert {:ok, %Book{} = book} = Budgeting.create_book(valid_attrs)
+  describe "create_book/1" do
+    test "with valid data creates a book" do
+      attrs = %{name: "some name"}
+
+      assert {:ok, %Book{} = book} = Budgeting.create_book(attrs)
       assert book.name == "some name"
     end
 
-    test "create_book/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Budgeting.create_book(@invalid_attrs)
+    test "with invalid data returns error changeset" do
+      attrs = %{name: nil}
+
+      assert {:error, changeset} = Budgeting.create_book(attrs)
+
+      errors = errors_on(changeset)
+
+      assert errors
+             |> Map.keys()
+             |> length() == 1
+
+      assert %{name: ["can't be blank"]} = errors
     end
+  end
 
-    test "update_book/2 with valid data updates the book" do
+  describe "update_book/2" do
+    test "with valid data updates the book" do
       book = insert(:book)
-      update_attrs = %{name: "some updated name"}
+      attrs = %{name: "some updated name"}
 
-      assert {:ok, %Book{} = book} = Budgeting.update_book(book, update_attrs)
+      assert {:ok, %Book{} = book} = Budgeting.update_book(book, attrs)
       assert book.name == "some updated name"
     end
 
-    test "update_book/2 with invalid data returns error changeset" do
+    test "with invalid data returns error changeset" do
       book = insert(:book)
-      assert {:error, %Ecto.Changeset{}} = Budgeting.update_book(book, @invalid_attrs)
-      assert book == Repo.get(Book, book.id)
-    end
+      attrs = %{name: nil}
 
-    test "delete_book/1 deletes the book" do
+      assert {:error, changeset} = Budgeting.update_book(book, attrs)
+
+      assert book == Repo.get(Book, book.id)
+
+      errors = errors_on(changeset)
+
+      assert errors
+             |> Map.keys()
+             |> length() == 1
+
+      assert %{name: ["can't be blank"]} = errors
+    end
+  end
+
+  describe "delete_book/1" do
+    test "deletes the given book" do
       book = insert(:book)
       assert {:ok, %Book{}} = Budgeting.delete_book(book)
       refute Repo.get(Book, book.id)
     end
+  end
 
-    test "change_book/1 returns a book changeset" do
+  describe "change_book/1" do
+    test "returns a book changeset" do
       book = insert(:book)
       assert %Ecto.Changeset{} = Budgeting.change_book(book)
     end
