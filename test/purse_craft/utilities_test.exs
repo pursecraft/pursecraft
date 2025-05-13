55260d8493e7fd7fdbf5515d504ab546dd5d1c5d
Terence Ponce <terence@pursecraft.com>
2025-05-13 08:58:20 +0800

    Remove comments from utilities test

--- test/purse_craft/utilities_test.exs
+++ test/purse_craft/utilities_test.exs
@@ -12,8 +12,6 @@ defmodule PurseCraft.UtilitiesTest do
     end
 
     test "preserves existing atom keys" do
-      # In Elixir, we can't mix atom and string keys in map literals,
-      # so we need to build the map in two steps
       initial_map = %{name: "Test Category"}
       input_with_string_key = Map.put(initial_map, "priority", 1)
       expected = %{name: "Test Category", priority: 1}
@@ -30,11 +28,9 @@ defmodule PurseCraft.UtilitiesTest do
     end
 
     test "only converts keys to existing atoms" do
-      # This should work for keys that correspond to existing atoms
       standard_input = %{"name" => "Test", "priority" => 1}
       assert %{name: "Test", priority: 1} = Utilities.atomize_keys(standard_input)
 
-      # This should raise for non-existing atoms
       assert_raise ArgumentError, fn ->
         Utilities.atomize_keys(%{"non_existent_atom_key" => "value"})
       end
