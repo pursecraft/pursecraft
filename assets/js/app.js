a5edcc9147b9a3ccbc5170dfb96fb1c3f371e1d0
Terence Ponce <terence@pursecraft.com>
2025-05-13 09:20:55 +0800

    Remove unused hook

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,8 +27,7 @@ import topbar from "../vendor/topbar"
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken},
-  hooks: {}
+  params: {_csrf_token: csrfToken}
 })
 
 // Show progress bar on live navigation and form submits
