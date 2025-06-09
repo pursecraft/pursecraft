f7d990d16425f474999d3835268e5258b9045f71
Terence Ponce <terence@pursecraft.com>
2025-06-09 13:46:59 +0800

    Add SubscribeCategory command

---
+++ test/purse_craft/budgeting/commands/pubsub/subscribe_category_test.exs
@@ -0,0 +1,17 @@
+defmodule PurseCraft.Budgeting.Commands.PubSub.SubscribeCategoryTest do
+  use PurseCraft.DataCase, async: true
+
+  alias PurseCraft.Budgeting.Commands.PubSub.SubscribeCategory
+
+  describe "call/1" do
+    test "subscribes to category PubSub channel" do
+      category_external_id = "category-123"
+
+      assert :ok = SubscribeCategory.call(category_external_id)
+
+      Phoenix.PubSub.broadcast(PurseCraft.PubSub, "category:category-123", {:test_message, "data"})
+
+      assert_receive {:test_message, "data"}
+    end
+  end
+end
