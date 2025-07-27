02f561bcc142d1d23bd5b0804d636716ad0a3b41
Terence Ponce <terence@pursecraft.com>
2025-07-27 16:33:20 +0800

    Fix dialyzer warnings

--- test/support/factories/search_factory.ex
+++ test/support/factories/search_factory.ex
@@ -6,18 +6,17 @@ defmodule PurseCraft.SearchFactory do
   alias PurseCraft.Search.Schemas.SearchToken
 
   def search_token_factory(attrs) do
-    default_attrs = %{
-      field_name: "name",
-      token_hash: "hel",
-      algorithm_version: 1,
-      token_length: 3
-    }
-
-    merged_attrs = Enum.into(attrs, default_attrs)
+    field_name = Map.get(attrs, :field_name, "name")
+    token_hash = Map.get(attrs, :token_hash, "hel")
 
     search_token =
       %SearchToken{}
-      |> SearchToken.changeset(merged_attrs)
+      |> SearchToken.changeset(%{
+        field_name: field_name,
+        token_hash: token_hash,
+        algorithm_version: 1,
+        token_length: 3
+      })
       |> Ecto.Changeset.apply_changes()
 
     search_token
