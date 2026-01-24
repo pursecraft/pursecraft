8ddee3a07f83570f47287702c2e5ea61488faafc
Terence Ponce <terence@pursecraft.com>
2026-01-24 14:20:26 +0800

    Exclude passwords from JSON encoding

--- apps/api/lib/purse_craft/contexts/identity/commands/create_user_password.ex
+++ apps/api/lib/purse_craft/contexts/identity/commands/create_user_password.ex
@@ -1,4 +1,5 @@
 defmodule PurseCraft.Identity.Commands.CreateUserPassword do
   @moduledoc false
+  @derive {Jason.Encoder, only: [:user_uuid]}
   defstruct [:user_uuid, :password, :password_confirmation]
 end
--- apps/api/lib/purse_craft/contexts/identity/commands/update_user_password.ex
+++ apps/api/lib/purse_craft/contexts/identity/commands/update_user_password.ex
@@ -1,4 +1,5 @@
 defmodule PurseCraft.Identity.Commands.UpdateUserPassword do
   @moduledoc false
+  @derive {Jason.Encoder, only: [:user_uuid]}
   defstruct [:user_uuid, :current_password, :new_password, :password_confirmation]
 end
