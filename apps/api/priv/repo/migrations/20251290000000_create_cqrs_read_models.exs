9638146ecec86ea5bcae922bb0586cc938f1e605
Terence Ponce <terence@pursecraft.com>
2026-01-29 11:05:49 +0800

    Change timestamp of migration

--- apps/api/priv/repo/migrations/20251290000000_create_cqrs_read_models.exs
+++ apps/api/priv/repo/migrations/20251290000000_create_cqrs_read_models.exs
@@ -4,7 +4,6 @@ defmodule PurseCraft.Repo.Migrations.CreateCqrsReadModels do
   def change do
     execute "CREATE EXTENSION IF NOT EXISTS citext", ""
 
-    # Create new_users table for CQRS read model
     create table(:new_users, primary_key: false) do
       add :id, :uuid, primary_key: true, null: false
       add :email, :citext, null: false
@@ -16,7 +15,6 @@ defmodule PurseCraft.Repo.Migrations.CreateCqrsReadModels do
 
     create unique_index(:new_users, [:email])
 
-    # Create new_users_tokens table for CQRS read model
     create table(:new_users_tokens, primary_key: false) do
       add :id, :uuid, primary_key: true, null: false
       add :user_id, references(:new_users, type: :uuid, on_delete: :delete_all), null: false
